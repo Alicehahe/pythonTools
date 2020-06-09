4f0afe2f9b1482bd382244f7223b157c074b3907
=Jane <907309377@qq.com>
2020-06-09 15:48:49 +0800

    这个脚本是用于计算基金卖出收益及收益率

---
+++ calFudIncome.py
@@ -0,0 +1,18 @@
+#coding:utf-8
+'''
+ @author:HanLu
+ @description:这段代码用于计算基金卖出时可获得收益及基金收益率
+ 
+'''
+
+
+jingzhiNow = input("请输入当前净值：")
+jingzhiBuy = input("请输入购买时净值：")
+buy_num = input("请输入购买的净值份额：")
+buy_money = input("请输入你购买花费金额：")
+rate = input("请输入卖出收费利率：")
+
+shouxufei = float(jingzhiNow)*float(buy_num)*float(rate)
+income = (float(jingzhiNow)-float(jingzhiBuy))*float(buy_num)
+print("您的收益为：",'%.2f' %(income-shouxufei),"元")
+print("您的收益率为：",('%.2f' %(income/int(buy_money)*100)),'%')
