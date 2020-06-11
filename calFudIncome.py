8f8dc739a60781a326214f5f8dfacb21f472c189
=Jane <907309377@qq.com>
2020-06-11 11:06:42 +0800

    version1.2 加入了循环控制，可重复输入

--- calFudIncome.py
+++ calFudIncome.py
@@ -5,14 +5,19 @@
  
 '''
 
+def test():
+    while 1:
+        jingzhiNow = input("请输入当前净值：")
+        jingzhiBuy = input("请输入购买时净值：")
+        buy_num = input("请输入购买的净值份额：")
+        buy_money = input("请输入你购买花费金额：")
+        rate = input("请输入卖出收费利率：")
 
-jingzhiNow = input("请输入当前净值：")
-jingzhiBuy = input("请输入购买时净值：")
-buy_num = input("请输入购买的净值份额：")
-buy_money = input("请输入你购买花费金额：")
-rate = input("请输入卖出收费利率：")
+        shouxufei = float(jingzhiNow)*float(buy_num)*float(rate)
+        income = (float(jingzhiNow)-float(jingzhiBuy))*float(buy_num)
+        print("您的收益为：",'%.2f' %(income-shouxufei),"元")
+        print("您的收益率为：",('%.2f' %(('.2f' %income)/int(buy_money)*100)),'%')
+        print('=============================')
 
-shouxufei = float(jingzhiNow)*float(buy_num)*float(rate)
-income = (float(jingzhiNow)-float(jingzhiBuy))*float(buy_num)
-print("您的收益为：",'%.2f' %(income-shouxufei),"元")
-print("您的收益率为：",('%.2f' %(income/int(buy_money)*100)),'%')
+if __name__ == '__main__':
+    test()
